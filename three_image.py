6e936d1ea206dfe21349b25548ca37c8503b7593
vkhripach <vkhripach@ivi.ru>
2021-06-16 11:41:16 +0400

    Второй тест, поиск статьи википедии

--- three_image.py
+++ three_image.py
@@ -24,4 +24,30 @@ class TestPython(unittest.TestCase):
                 ivi_url_count += 1
         ivi_url_min = 2
         # Проверяем, что их не меньше 3
-        assert ivi_url_count > ivi_url_min
+        assert ivi_url_count > ivi_url_min, 'Количество картинок, ведущих на ivi меньше 3'
+
+    def test_wiki_go_to_ivi(self):
+        # Указываем путь до chromedriver
+        self.driver = webdriver.Chrome('C:\chromedriver_win32\chromedriver.exe')
+        self.driver.implicitly_wait(30)
+        driver = self.driver
+        # Открываем необходимый урл
+        driver.get("https://www.google.com/")
+        # Вводим значение ivi в поисковую строку, нажимаем enter
+        driver.find_element_by_class_name("gLFyf").send_keys("ivi")
+        driver.find_element_by_class_name("gLFyf").send_keys(Keys.ENTER)
+        # Ищем статью на википедию про ivi на первых 5 страницах
+        for i in range(5):
+            try:
+                driver.find_element_by_xpath("//a[@href='https://ru.wikipedia.org/wiki/Ivi.ru']").click()
+                if driver.current_url == 'https://ru.wikipedia.org/wiki/Ivi.ru':
+                    break
+            except:
+                print('Не нашли ссылку на википедию про ivi на странице', i+1)
+            # Кликаем на следующую страницу в поисковом запросе
+            driver.find_element_by_css_selector('[aria-label="Page '+str(i+2)+'"]').click()
+        # Проверяем, что нашли статью на википедию про ivi и перешли на неё
+        assert driver.current_url == 'https://ru.wikipedia.org/wiki/Ivi.ru', \
+            'Не нашли ссылку на википедию про ivi на первых 5 страницах'
+        # Проверяем, что в статье википедии про ivi есть ссылка на официальный сайт ivi
+        assert driver.find_element_by_xpath("//a[@href='http://ivi.ru']"), 'Не нашли ссылку на ivi в статье википедии'
